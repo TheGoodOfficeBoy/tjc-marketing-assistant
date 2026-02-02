49ec3c8d5401bffa86c0d9e77eb4321e3d1af81c
TheGoodOfficeBoy <153488402+TheGoodOfficeBoy@users.noreply.github.com>
2026-02-02 17:48:14 +0700

    Refactor Firebase configuration and initialization
    Updated Firebase SDK imports and restructured firebaseConfig export.

--- firebase.js
+++ firebase.js
@@ -1,10 +1,16 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+export const firebaseConfig = {
   apiKey: "AIzaSyAuYStMuXj-S9kItbQASZUKJ5NvhokpeVc",
   authDomain: "tjc-marketing-console.firebaseapp.com",
   projectId: "tjc-marketing-console",
   storageBucket: "tjc-marketing-console.firebasestorage.app",
   messagingSenderId: "896679016286",
-  appId: "1:896679016286:web:599a88c1ad32943f1cc1cf",
-  measurementId: "G-LK4CG6NJD0"
+  appId: "1:896679016286:web:599a88c1ad32943f1cc1cf"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
